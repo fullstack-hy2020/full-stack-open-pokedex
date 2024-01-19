d4ba7ff0728e1e4ea328c41b411624091ec41c31
WMCS50 <walid.muslih@gmail.com>
2024-01-18 17:27:18 -0700

    11.8

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-// eslint-disable-next-line no-undef
-ReactDOM.createRoot(<App />, document.getElementById('app'))
+// eslint-disable-next-line no-undef, react/no-deprecated
+ReactDOM.render(<App />, document.getElementById('app'))
