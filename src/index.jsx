ed3d27f610c16fce89679e20abdcf39bd679e4a3
WMCS50 <walid.muslih@gmail.com>
2024-01-18 17:21:57 -0700

    11.8

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
 // eslint-disable-next-line no-undef
-ReactDOM.createRoot(document.getElementById('app')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(<App />, document.getElementById('app'))
