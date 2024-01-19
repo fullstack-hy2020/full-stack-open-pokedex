7f860c792ed2742327a29e8d93862ada6ed8729d
WMCS50 <walid.muslih@gmail.com>
2024-01-19 11:10:21 -0700

    11.10-12

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
-})
+}) */
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
@@ -19,6 +19,11 @@ app.get('/version', (req, res) => {
   res.send('ok')
 }) */
 
+// start app in a wrong port
+app.listen(PORT + 1, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
