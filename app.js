c13364cbac2dbaea1a471eb4e52f1cad51f212ce
Aigar Voog <aigar.voog@outlook.com>
2024-01-26 12:25:11 +0200

    exc fly 11.10-11.11

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -9,4 +10,8 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
