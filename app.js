c70af64a35f0da27b2b15526ab6da1192305fdc9
Aigar Voog <aigar.voog@outlook.com>
2024-01-26 19:44:42 +0200

    yes yes

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT +1, () => {
+fail build
+
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
