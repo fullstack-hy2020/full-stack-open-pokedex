8b6313440713ba44833ae20e49458aeb14099dcf
Aigar Voog <aigar.voog@outlook.com>
2024-01-26 12:43:47 +0200

    exc 11.2 wrong port

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  // if (true) throw('error...  ')
+  res.send('ok')
 })
