9c6f3be0c7620537622f72d3849a7d702cf7f419
Emad Khan <contactemadkhan@gmail.com>
2024-01-30 14:32:11 -0500

    add header

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom'
 
 const PokemonList = ({ pokemonList }) => {
   return (
-    <div className="list-container">
-      {pokemonList.map(({ id, name }) => (
-        <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
-          <div
-            className="list-item-name"
+    <>
+      <h1>Pokemon</h1>
+      <div className="list-container">
+        {pokemonList.map(({ id, name }) => (
+          <Link
+            key={id}
+            to={`/pokemon/${name}`}
+            className="list-item"
+            style={{
+              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
+            }}
           >
-            {name}
-          </div>
-        </Link>
-      ))}
-    </div>
+            <div className="list-item-name">{name}</div>
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
 
