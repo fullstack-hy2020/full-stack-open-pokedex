907203d33803b9670eeed504e5c6b8ddff7bf5db
Emad Khan <contactemadkhan@gmail.com>
2024-01-30 11:39:37 -0500

    add cypress

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
+  console.log(`node env ${process.env.NODE_ENV}`)
   console.log(`server started on port ${PORT}`)
 })
