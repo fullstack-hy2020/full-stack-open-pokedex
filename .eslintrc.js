39f54317f7314715e1770a650cac247e7d2fdfa4
Ari Kankainen <ari.kankainen@saunalahti.fi>
2024-02-10 18:35:04 +0200

    removed linebreak rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
