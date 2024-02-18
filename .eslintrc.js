2e77197ca3b1e22c5bee3199c74d591c8bfb0005
aungmyokhant2465 <aungmyokhant2465@gmail.com>
2024-02-18 23:41:08 +0630

    cypress e2e test

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es6: true,
     "jest/globals": true,
     node: true,
+    "cypress/globals": true
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
@@ -13,10 +14,10 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
+    "linebreak-style": ["error", "windows"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",
