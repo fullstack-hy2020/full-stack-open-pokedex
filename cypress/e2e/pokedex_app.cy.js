e71160458802101145c54baf3f79947d4bf97e69
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-22 20:51:38 -0800

    click item on page

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -5,3 +5,17 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokemon Navigation', () => {
+  it('navigates to a specific Pokemon page and checks content', () => {
+    // Visit the main page
+    cy.visit('http://localhost:5000')
+
+    // Click on the link for Ivysaur
+    cy.contains('ivysaur').click()
+
+    // Check that the Ivysaur page has loaded and contains the word 'chlorophyll'
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll').should('exist')
+  })
+})
