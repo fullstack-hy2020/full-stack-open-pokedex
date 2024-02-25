e6782563ef2819ae15582e1690ac5c04c0fe3b68
Lucas281433 <lucas2814dev@gmail.com>
2024-02-25 10:29:18 -0300

    Prepare to render

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line no-undef
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 // get the port from env variable
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.use(express.static('dist'))
 
