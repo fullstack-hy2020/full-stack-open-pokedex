56c36cb9bf73f00e97f83cdc7c4fd2ffef549e67
marcogiabbani <mtgiabbani@gmail.com>
2024-02-26 09:17:55 -0300

    back to dist

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Serve static files from the "dist" directory
-app.use(express.static('./dist', { index: 'index.html' }))
-
-// Optional: Define a route handler for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
