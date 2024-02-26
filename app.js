c0600aa8118479f429eaa5590f8d9e2b401bc1b1
marcogiabbani <mtgiabbani@gmail.com>
2024-02-26 08:57:14 -0300

    explicit dist file use

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable
+// get the port from the env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+// Serve static files from the "dist" directory
+app.use(express.static('dist', { index: 'index.html' }))
+
+// Optional: Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
