e2c85cdf5e5468afd7ef560e54f31b81d22091fe
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-03-02 11:29:00 -0300

    Fixed code errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jest/globals': true
+    'jest/globals': true,
+    'node': true
   },
   'extends': [
     'eslint:recommended',
@@ -36,6 +37,7 @@ module.exports = {
       'never'
     ],
     'eqeqeq': 'error',
+    'no-console': 'off',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': [
       'error', 'always'
@@ -43,10 +45,10 @@ module.exports = {
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
-    'no-console': 'error',
     'react/prop-types': 0
   },
   'globals': {
-    'module': 'readonly'
+    'module': 'readonly',
+    'process': 'readonly'
   }
 }
