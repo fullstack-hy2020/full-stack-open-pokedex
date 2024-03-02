220ddc75dbb880b72b1604c98d0a0444cf673bfc
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-03-02 12:31:08 -0300

    Changed stuff

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress")
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://localhost:5000'
   },
 })
