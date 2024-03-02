ea35347350230f9b210595478e062a87882dee3c
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-03-02 15:40:21 -0300

    A "broken" deploy

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -3,6 +3,11 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000/')
   })
 
+  it('it\'s healthy', () => {
+    cy.visit('http://localhost:5000/healty')
+    cy.contains('ok')
+  })
+
   it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
