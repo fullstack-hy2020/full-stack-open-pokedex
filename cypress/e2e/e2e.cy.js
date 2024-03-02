8c9a3adc7ef7a0f80ec8018dfcd22951272a251e
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-03-02 12:20:04 -0300

    E2E tests

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,24 @@
+describe('Pokedex', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:8080/')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can interact with front page', () => {
+    cy.contains('ivysaur').click()
+    cy.get('*[class^="pokemon-page"]')
+    cy.get('*[class^="pokemon-abilities"]')
+  })
+
+  it('can go interact with buttons', () => {
+    cy.contains('ivysaur').click()
+    cy.contains('Next').click()
+    cy.contains('Previous').click()
+    cy.contains('Home').click()
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
