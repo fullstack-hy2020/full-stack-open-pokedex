b63c416abfe5a0b6df790de966858b596d1a4cd1
Tturna <eemelijuponaho@gmail.com>
2024-03-04 20:01:27 +0200

    exercise 11.12

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.get('/health', (_req, res) => {
+  if (app) throw('error...  ')
+  res.send('ok')
+})
+
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
