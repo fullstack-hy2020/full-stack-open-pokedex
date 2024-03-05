3b4290e9e0e1716d3256dc1c24ba810b3da9ef96
agold95 <112718049+agold95@users.noreply.github.com>
2024-03-05 13:06:52 -0500

    finished part 11.9

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,13 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('ivysaur can be visited', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
