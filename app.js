b24a2b7154728660299fcae288ca4b8e29a5a136
SamkHarks <samikh90@gmail.com>
2024-03-07 17:46:02 +0200

    fail again

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  res.send('ok')
+  return res.status(500)
+  //res.send('ok')
 })
 
 app.listen(PORT, () => {
