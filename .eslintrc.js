a31d8e4928a9a7e331590720c186292af7951649
KarolKarnas <karol.karnas@gmail.com>
2024-03-07 11:48:47 +0100

    11.9_2 Simple end to end tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     'node': true,
     'browser': true,
     'es6': true,
-    'jest/globals': true,
-    "cypress/globals": true
+    'jest/globals': true
+    // "cypress/globals": true
   },
   'extends': [
     'eslint:recommended',
