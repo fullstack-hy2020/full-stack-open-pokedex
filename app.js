47b572eeb13db5e64020b2e00c14d7aafc4c5c65
KarolKarnas <karol.karnas@gmail.com>
2024-03-08 10:03:23 +0100

    11.12 Health check - break

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
