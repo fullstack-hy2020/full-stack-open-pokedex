9924ea69cff8b06e0f0e569a264d09dc4e074738
dermotbg <dermotbateman@gmail.com>
2024-03-09 11:44:00 +0200

    done: 11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,5 +3,10 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('can be navigated to a specific pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('div').contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
