16a876f2e605f39e31a83a5584b1d024be07da24
KarolKarnas <karol.karnas@gmail.com>
2024-03-11 09:52:44 +0100

    Add new Title component
    - utilize it in the PokemonList.jsx
    - add style for the Title component in styles.css

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Title from './Title'
 
 const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
         <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
+          <Title  title={name}/>
           <div
             className="list-item-name"
           >
---
+++ src/Title.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Title = ({ title }) => {
+  return <div className="pokemon-title">{title}</div>
+}
+export default Title
