e6f3cc940d5c6dbd2dadcea209f4eb6634f7c5a8
dermotbg <dermotbateman@gmail.com>
2024-03-12 08:45:51 +0200

    test: breaking healthcheck

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
