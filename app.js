0fef19e6efad217b063ec10eb438597981d0a697
Vintersorganic <jorge.gdp@gmail.com>
2024-03-17 19:53:15 +0100

    .

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const path = require('path')
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
+
 
 app.get('/health', (req, res) => {
   res.send('ok')
