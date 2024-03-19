0b43a8ec7d44af3dd3d204932ba0dff509506a06
Yaups <tpallison.ta@gmail.com>
2024-03-19 14:53:09 +0000

    Exercise 11.9 completed

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('individual pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('arbok').click()
+    cy.contains('intimidate')
+    cy.contains('unnerve')
+  })
 })
