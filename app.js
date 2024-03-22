737d7d2af0f24ca6501f2876ced10f54a19f8ced
Yaups <tpallison.ta@gmail.com>
2024-03-22 10:01:25 +0000

    canary test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('broken #3')
+  res.send('canary')
 })
 
 app.get('/health', () => {})
