6d0605ff249cca3258f5176ea70a58309ba37c20
Yaups <tpallison.ta@gmail.com>
2024-03-22 09:51:06 +0000

    aaaaa

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('still broken')
+  res.send('broken #3')
 })
 
 app.get('/health', () => {})
